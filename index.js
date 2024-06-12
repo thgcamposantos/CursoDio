25ce89eb42a8d42b1801037b5cea63c04659703d
thgcamposantos <thgcamposantos@gmail.com>
2024-06-12 20:47:42 -0300

    final do projeto

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello Word');
+let nome = prompt("Nome do Herói: ");
+let Xp = parseInt(prompt('Qual a XP do Herói: '));
+let nivel = null;
+    if(Xp <= 1000){
+        nivel = 'Ferro';
+    }else if(Xp > 1000 && Xp <= 2000){
+        nivel = 'Bronze'
+    }else if(Xp > 2000 && Xp <= 5000){
+        nivel = 'Prata'
+    }else if(Xp > 5000 && Xp <= 7000){
+        nivel = 'Ouro';
+    }else if(Xp > 7000 && Xp <= 8000){
+        nivel = 'Platina';
+    }else if(Xp > 8000 && Xp <= 9000){
+        nivel = 'Ascedente'
+    }else if(Xp > 9000 && Xp <= 10000){
+        nivel = 'Imortal'
+    }else{
+        nivel = 'Radiante'
+    }
+
+console.log('O herói de nome ' + nome + " está no nível de " + nivel + " com um XP de:" + Xp);
